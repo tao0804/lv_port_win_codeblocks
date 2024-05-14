5b4723eee67dedb62a670262078ba1cb64b4966a
hao <33110820@qq.com>
2024-05-15 00:40:58 +0800

    更新：增加按键回调，cfg值可配置

--- app/my_gui.c
+++ app/my_gui.c
@@ -111,6 +111,44 @@ static void my_gui_battery_init(void)
     lv_anim_start(&a);                             /*Start the animation*/
 }
 
+struct my_gui_config_value
+{
+    uint16_t cfg_range;
+    uint16_t cfg_value;
+    char ** cfg_str;
+};
+
+char *cfg_freq[] = {"2430MHz", "2440MHz", "2450MHz"};
+char *cfg_rate[] = {"100Kbps","500Kbps","1Mbps"};
+char *cfg_percent[] = {"0\%", "10\%", "20\%", "30\%", "40\%", "50\%", "60\%", "70\%", "80\%", "90\%", "100\%"};
+
+static void my_gui_btn_cb(lv_event_t * e)
+{
+    lv_event_code_t event_code = lv_event_get_code(e);
+    lv_obj_t *btn = lv_event_get_target(e);
+    uint32_t key = *(uint32_t*)lv_event_get_param(e);
+    lv_obj_t *label = lv_obj_get_user_data(btn);    // 获取label对象
+    struct my_gui_config_value *cfg = lv_obj_get_user_data(label);    // 获取cfg
+
+    if (event_code != LV_EVENT_KEY) {
+        return; // 本回调只处理KEY事件
+    }
+
+    if (key == LV_KEY_RIGHT) {
+        // 增大配置项
+        if (cfg->cfg_value + 1 < cfg->cfg_range) {
+            cfg->cfg_value++;
+            lv_label_set_text(label, cfg->cfg_str[cfg->cfg_value]);
+        }
+    } else if (key == LV_KEY_LEFT) {
+        // 减小配置项
+        if (cfg->cfg_value > 0) {
+            cfg->cfg_value--;
+            lv_label_set_text(label, cfg->cfg_str[cfg->cfg_value]);
+        }
+    }
+}
+
 static void my_gui_tabview_init(void)
 {
     /*Create a Tab view object*/
@@ -144,16 +182,16 @@ static void my_gui_tabview_init(void)
     lv_obj_t * btn3;
     lv_obj_t * btn4;
     btn1 = lv_list_add_btn(list1, NULL, "Freq:");
-    lv_obj_add_event_cb(btn1, NULL, LV_EVENT_ALL, NULL);
+    lv_obj_add_event_cb(btn1, my_gui_btn_cb, LV_EVENT_KEY, NULL);
 
     btn2 = lv_list_add_btn(list1, NULL, "Rate:");
-    lv_obj_add_event_cb(btn2, NULL, LV_EVENT_ALL, NULL);
+    lv_obj_add_event_cb(btn2, my_gui_btn_cb, LV_EVENT_KEY, NULL);
 
     btn3 = lv_list_add_btn(list1, NULL, "Volume:");
-    lv_obj_add_event_cb(btn3, NULL, LV_EVENT_ALL, NULL);
+    lv_obj_add_event_cb(btn3, my_gui_btn_cb, LV_EVENT_KEY, NULL);
 
     btn4 = lv_list_add_btn(list1, NULL, "Brightness:");
-    lv_obj_add_event_cb(btn4, NULL, LV_EVENT_ALL, NULL);
+    lv_obj_add_event_cb(btn4, my_gui_btn_cb, LV_EVENT_KEY, NULL);
 
     // 创建group
     lv_group_t * group1 = lv_group_create();
@@ -163,6 +201,44 @@ static void my_gui_tabview_init(void)
     lv_group_add_obj(group1, btn3);
     lv_group_add_obj(group1, btn4);
     lv_group_set_default(group1);   // 设置为默认组
+
+    // 创建config值 label
+    static lv_obj_t * label1;
+    static struct my_gui_config_value cfg1 = {.cfg_range = ARRAYSIZE(cfg_freq), .cfg_value = 1, cfg_freq};
+    label1 = lv_label_create(btn1);
+    lv_label_set_text(label1, cfg1.cfg_str[cfg1.cfg_value]);
+    lv_label_set_long_mode(label1, LV_LABEL_LONG_SCROLL_CIRCULAR);
+    lv_obj_set_flex_grow(label1, 1);
+    lv_obj_set_user_data(btn1, label1); // 将label对象关联到btn对象，减少全局变量使用
+    lv_obj_set_user_data(label1, &cfg1); // 将cfg数据关联到label对象，减少全局变量使用
+
+    static lv_obj_t * label2;
+    static struct my_gui_config_value cfg2 = {.cfg_range = ARRAYSIZE(cfg_rate), .cfg_value = 1, cfg_rate};
+    label2 = lv_label_create(btn2);
+    lv_label_set_text(label2, cfg2.cfg_str[cfg2.cfg_value]);
+    lv_label_set_long_mode(label2, LV_LABEL_LONG_SCROLL_CIRCULAR);
+    lv_obj_set_flex_grow(label2, 1);
+    lv_obj_set_user_data(btn2, label2);
+    lv_obj_set_user_data(label2, &cfg2);
+
+    static lv_obj_t * label3;
+    static struct my_gui_config_value cfg3 = {.cfg_range = ARRAYSIZE(cfg_percent), .cfg_value = 5, cfg_percent};
+    label3 = lv_label_create(btn3);
+    lv_label_set_text(label3, cfg3.cfg_str[cfg3.cfg_value]);
+    lv_label_set_long_mode(label3, LV_LABEL_LONG_SCROLL_CIRCULAR);
+    lv_obj_set_flex_grow(label3, 1);
+    lv_obj_set_user_data(btn3, label3);
+    lv_obj_set_user_data(label3, &cfg3);
+
+    static lv_obj_t * label4;
+    static struct my_gui_config_value cfg4 = {.cfg_range = ARRAYSIZE(cfg_percent), .cfg_value = 5, cfg_percent};
+    label4 = lv_label_create(btn4);
+    lv_label_set_text(label4, cfg4.cfg_str[cfg4.cfg_value]);
+    lv_label_set_long_mode(label4, LV_LABEL_LONG_SCROLL_CIRCULAR);
+    lv_obj_set_flex_grow(label4, 1);
+    lv_obj_set_user_data(btn4, label4);
+    lv_obj_set_user_data(label4, &cfg4);
+
 #endif
 
 #if 1
